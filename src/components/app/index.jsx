9a1102704d0dd64116c2fcfbc9c7130c1c223457
grib9544 <grib9544@gmail.com>
2022-02-20 22:08:00 +0300

    implement burger-ingredients component

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,11 +1,26 @@
-import { AppHeader } from '../app-header'
 
-function App() {
+import React from 'react';
+import { AppHeader } from '../app-header'
+import styles from './app.module.css'
+import { BurgerIngredients } from '../burger-ingredients';
+
+export const App = () => {
+
   return (
     <>
       <AppHeader />
+      <main className={styles.main}>
+        	<h1 className="text text_type_main-large mb-5">
+          		Соберите бургер
+       		</h1>
+        	<div className={styles.main__content}>
+				<BurgerIngredients />
+          		<div className={styles.main__tab2}>
+            		lol
+          		</div>
+        	</div>
+      </main>
     </>
   );
 }
 
-export default App;
---
+++ src/components/burger-ingredients/index.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styles from './burger-ingredients.module.css'
+import { IngredientCard } from './ingredient-card'
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
+import data from '../../utils/data.json'
+
+export const BurgerIngredients = () => {
+    const [current, setCurrent] = React.useState('bun')
+
+    return (
+        <section className={styles.ingredients}>
+            <div className={styles.ingredients__tab}>
+                <Tab value="bun" active={current === "bun"} onClick={setCurrent}>
+                    Булки
+                </Tab>
+                <Tab value="souse" active={current === "souse"} onClick={setCurrent}>
+                    Соусы
+                </Tab>
+                <Tab value="main" active={current === "main"} onClick={setCurrent}>
+                    Начинки
+                </Tab>
+            </div>
+            {/* 
+                TODO: to separate component
+             */}
+            <div className={styles.scrollable_content}>
+                <h2 className="text text_type_main-medium">
+                    Булки
+                </h2>
+                <ul className={styles.ingredients__list}>
+                    {data.filter(ing => ing.type === 'bun').map(ing => (
+                        <li>
+                            <IngredientCard 
+                                key={ing._id} 
+                                img={ing.image} 
+                                price={ing.price} 
+                                title={ing.name} 
+                            />
+                        </li>
+                    ))}
+                </ul>
+                <h2 className="text text_type_main-medium">
+                    Соусы
+                </h2>
+                <ul className={styles.ingredients__list}>
+                    {data.filter(ing => ing.type === 'sauce').map(ing => (
+                        <li>
+                            <IngredientCard 
+                                key={ing._id} 
+                                img={ing.image} 
+                                price={ing.price} 
+                                title={ing.name} 
+                            />
+                        </li>
+                    ))}
+                </ul>
+                <h2 className="text text_type_main-medium">
+                    Ингредиенты
+                </h2>
+                <ul className={styles.ingredients__list}>
+                    {data.filter(ing => ing.type === 'main').map(ing => (
+                        <li>
+                            <IngredientCard 
+                                key={ing._id} 
+                                img={ing.image} 
+                                price={ing.price} 
+                                title={ing.name} 
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
+}
