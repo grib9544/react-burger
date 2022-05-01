91af91a314bec8a329fd1676e5ca6dd25da1d246
grib9544 <grib9544@gmail.com>
2022-05-02 01:33:52 +0400

    add counter to ingredient card component

--- src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
+++ src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
@@ -1,7 +1,7 @@
 import styles from './ingredient-card.module.css'
 import PropTypes from 'prop-types';
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import React, { useContext } from 'react';
+import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components'
+import React, { useContext, useState, useEffect } from 'react';
 import { IngredientDetails } from '../../ingredient-details/ingredient-details';
 import { Modal } from '../../modal/modal';
 import { ConstructorContext } from '../../../services/contexts/constructor';
@@ -10,7 +10,13 @@ import { randomAlphaNumeric } from '../../../utils'
 export const IngredientCard = (props) => {
     const { constrState, constrDispatcher } = useContext(ConstructorContext)
 
-    const [visibility, setVisibility] = React.useState(false)
+    const [count, setCount] = useState(0)
+    const [visibility, setVisibility] = useState(false)
+
+    useEffect(() => {
+        const ingredientCount = constrState.burger.filter(ing => ing._id === props._id).length
+        setCount(ingredientCount)
+    }, [constrState.burger, props._id])
     
     const onClick = () => {
         constrDispatcher({ type: 'SET_INGREDIENT', payload: {...props, constrId: randomAlphaNumeric()}})
@@ -25,6 +31,7 @@ export const IngredientCard = (props) => {
                 </Modal> )
             }
             <div className={styles.ingredient} onClick={onClick}>
+                {!!count && <Counter count={count} size="default" />}
                 <img src={props.image} alt={props.name} className={styles.ingredient__img} />
                 <div className={styles.ingredient__price}>
                     <span className='text text_type_digits-default'>
